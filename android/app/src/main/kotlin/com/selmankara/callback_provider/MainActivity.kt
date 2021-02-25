a498859962f103f9d3f3ad4402bbc60be4c5a502
selmankara <selman@selmankara.com>
2021-02-26 01:11:05 +0300

    v1

---
+++ android/app/src/main/kotlin/com/selmankara/callback_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.selmankara.callback_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:callback_provider/state_data.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(Provider<StateData>(
+    create: (BuildContext context) {
+      return StateData();
+    },
+    child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('callback kullanımı'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(child: SolWidgetA()),
+            Expanded(child: SagWidgetA())
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SolWidgetA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String sehir = Provider.of<StateData>(context).sehir;
+    return Container(
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Text(
+              'Sol Widget',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'Sehir: $sehir',
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ));
+  }
+}
+
+class SagWidgetA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Column(children: [
+        Text(
+          'SagWidget A',
+          style: TextStyle(fontSize: 20),
+        ),
+        SagWidgetB()
+      ]),
+    );
+  }
+}
+
+class SagWidgetB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 180,
+      color: Colors.purple,
+      child: Column(children: [
+        Text(
+          'SagWidget B',
+          style: TextStyle(fontSize: 20),
+        ),
+        SagWidgetC()
+      ]),
+    );
+  }
+}
+
+class SagWidgetC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String sehir = Provider.of<StateData>(context).sehir;
+    return Container(
+      color: Colors.white,
+      height: 150,
+      width: 160,
+      child: Column(children: [
+        Text(
+          'SagWidget C',
+          style: TextStyle(fontSize: 20),
+        ),
+        Text(
+          'Şehir: $sehir ',
+          style: TextStyle(fontSize: 20),
+        ),
+        TextField(onChanged: null)
+      ]),
+    );
+  }
+}
